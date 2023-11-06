7828756d5c3ffcbea4d156c002203ab3075e87d1
TomEelmna <Tomeelman33@gmail.com>
2023-11-06 16:41:27 +0100

    Done

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,127 +1,44 @@
-﻿namespace Calculator
+﻿namespace CalculatorProgram
 {
-    class Calculator
+    public class Calculate
     {
-        static void Main(string[] args)
+        public decimal PerformCalculation(decimal num1, decimal num2, char oper)
         {
-            bool proceed = true;
-            decimal num1 = 0;
-            decimal num2 = 0;
+            decimal result = 0;
+            bool valid = true;
 
-            while (proceed)
+            switch (oper)
             {
-                do
-                {
-                    Console.WriteLine("Enter first number");
-                    string inputUser1 = Console.ReadLine();
-
-                    if (string.IsNullOrEmpty(inputUser1))
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
                     {
-                        Console.WriteLine("You need to fill in the first number!!");
-                        Console.WriteLine("");
+                        valid = false;
                     }
                     else
                     {
-                        if (decimal.TryParse(inputUser1, out num1))
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("");
-                            Console.WriteLine("This input needs to be a valid number!!");
-                            Console.WriteLine("");
-                        }
+                        result = num1 / num2;
                     }
-                }
-                while (true);
-
-                do
-                {
-                    Console.WriteLine("Enter second number");
-                    string inputUser2 = Console.ReadLine();
-
-                    if (string.IsNullOrEmpty(inputUser2))
-                    {
-                        Console.WriteLine("You need to fill in the second number!!");
-                        Console.WriteLine("");
-                    }
-                    else
-                    {
-                        if (decimal.TryParse(inputUser2, out num2))
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("");
-                            Console.WriteLine("This input needs to be a valid number!!");
-                            Console.WriteLine("");
-                        }
-                    }
-                }
-                while (true);
-
-                Console.WriteLine("Choose operator: +, -, /, *");
-                char oper = Convert.ToChar(Console.ReadLine());
-
-                if (oper == '+' || oper == '-' || oper == '/' || oper == '*')
-                {
-                    switch (oper)
-                    {
-                        case '+':
-                            decimal sum_add = num1 + num2;
-                            decimal sum_add_roundedup = Math.Round(sum_add, 2);
-                            Console.WriteLine("Result: " + sum_add_roundedup);
-                            break;
-                        case '-':
-                            decimal sum_min = num1 - num2;
-                            decimal sum_min_roundedup = Math.Round(sum_min, 2);
-                            Console.WriteLine("Result: " + sum_min_roundedup);
-                            break;
-                        case '/':
-                            if (num2 == 0)
-                            {
-                                Console.WriteLine("Cannot divide by 0");
-                            }
-                            else
-                            {
-                                decimal sum_div = num1 / num2;
-                                decimal sum_div_roundedup = Math.Round(sum_div, 2);
-                                Console.WriteLine("Result: " + sum_div_roundedup);
-                            }
-                            break;
-                        case '*':
-                            decimal sum_mul = num1 * num2;
-                            decimal sum_mul_roundedup = Math.Round(sum_mul, 2);
-                            Console.WriteLine("Result: " + sum_mul_roundedup);
-                            break;
-                    }
-                }
-                else
-                {
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                default:
                     Console.WriteLine("Invalid operator");
-                }
-
-                Console.WriteLine("To proceed, type 'y' or 'yes', or 'n' or 'no' to stop:");
-                string confirmation = Console.ReadLine().ToLower();
-
-                if (confirmation == "y" || confirmation == "yes")
-                {
-                    proceed = true;
-                }
-                else if (confirmation == "n" || confirmation == "no")
-                {
-                    proceed = false;
-                    Console.WriteLine("Calculator stopped.");
-                    System.Threading.Thread.Sleep(2000);
-                }
-                else
-                {
-                    Console.WriteLine("Invalid confirmation, Stopping calculator.");
-                    System.Threading.Thread.Sleep(2000);
-                    Environment.Exit(0);
-                }
+                    break;
+            }
+            if (valid)
+            {
+                return result;
+            }
+            else
+            {
+                return -469864871481296421;
             }
         }
     }
---
+++ Calculator/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool proceed = true;
+            decimal num1;
+            decimal num2;
+
+            while (proceed)
+            {
+                do
+                {
+                    Console.WriteLine("Enter first number");
+                    string inputUser1 = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(inputUser1))
+                    {
+                        Console.WriteLine("You need to fill in the first number!!");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        if (decimal.TryParse(inputUser1, out num1))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("This input needs to be a valid number!!");
+                            Console.WriteLine("");
+                        }
+                    }
+                } while (true);
+
+                do
+                {
+                    Console.WriteLine("Enter second number");
+                    string inputUser2 = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(inputUser2))
+                    {
+                        Console.WriteLine("You need to fill in the second number!!");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        if (decimal.TryParse(inputUser2, out num2))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("This input needs to be a valid number!!");
+                            Console.WriteLine("");
+                        }
+                    }
+                } while (true);
+
+                Console.WriteLine("Choose operator: +, -, /, *");
+                char oper = Convert.ToChar(Console.ReadLine());
+
+                Calculate calculator = new Calculate();
+                decimal result = calculator.PerformCalculation(num1, num2, oper);
+                decimal result_rounded = Math.Round(result, 2);
+
+                if (result == -469864871481296421)
+                {
+                    Console.WriteLine("Cannot divide by 0 !!");
+                }
+                else
+                {
+                    Console.WriteLine("Result: " + result_rounded);
+                }
+
+                Console.WriteLine("To proceed, type 'y' or 'yes', or 'n' or 'no' to stop:");
+                string confirmation = Console.ReadLine().ToLower();
+
+                if (confirmation == "y" || confirmation == "yes")
+                {
+                    proceed = true;
+                }
+                else if (confirmation == "n" || confirmation == "no")
+                {
+                    proceed = false;
+                    Console.WriteLine("Calculator stopped.");
+                    System.Threading.Thread.Sleep(2000);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid confirmation, Stopping calculator.");
+                    System.Threading.Thread.Sleep(2000);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
